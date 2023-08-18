b00efee0f876571fc6134e6d0a3f6eb0c807be42
zslx <1462240522@qq.com>
2023-08-18 16:11:51 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  basePath: '/gh-pages-test',
 });
 
 const nextConfig = {
-  output: "export", // 打包模式
   images: {
     unoptimized: true,
   },
