bd341a4ed60ddf910c59eb1344b88a6530dfbfc9
zslx <1462240522@qq.com>
2023-08-18 12:02:16 +0800

    first commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,8 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/zslx20201005/Nextra-blog',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
   },
