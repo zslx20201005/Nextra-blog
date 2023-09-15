7256b1b314cd80510259a068818c79027b93b90b
zslx <1462240522@qq.com>
2023-09-15 15:48:34 +0800

    team

--- components/customCard/index.tsx
+++ components/customCard/index.tsx
@@ -6,7 +6,7 @@ export default function CustomCard({ title, img }) {
   return (
     <Card icon="" image={img} title={title} href="">
       <Image src={img} alt="Hello" width={10} height={80}
-        className="rounded-full w-24 h-28 border-solid border-2 border-sky-500 bg-cover" />
+        className="rounded-full w-24 h-28 bg-cover" />
     </Card>
   )
 }
