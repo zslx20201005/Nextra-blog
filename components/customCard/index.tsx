0f078c5b98ef487baec55d94ee10f86f8f345a1c
zslx <1462240522@qq.com>
2023-09-15 15:48:00 +0800

    team

--- components/customCard/index.tsx
+++ components/customCard/index.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 export default function CustomCard({ title, img }) {
   return (
     <Card icon="" image={img} title={title} href="">
-      <Image src={img} alt="Hello" width={20} height={80}
-        className="rounded-full w-20 h-full bg-cover" />
+      <Image src={img} alt="Hello" width={10} height={80}
+        className="rounded-full w-24 h-28 border-solid border-2 border-sky-500 bg-cover" />
     </Card>
   )
 }
